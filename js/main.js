51d145f775729bb6a7673cd3dd9f04adb0e970ae
dalmarox <cersosimodalma@gamil.com>
2022-11-21 12:36:24 -0300

    1er commit curso JS

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let ingresoDato= prompt(" Ingresa tu edad");
+
+console.log(ingresoDato);
+
+let nombreyapellido= prompt("Nombre y Apellido");
+
+console.log(nombreyapellido);
+let medicamento=prompt("droga")
+console.log(medicamento);
+let medicamentoregistrado=prompt("NR");
+console.log(medicamentoregistrado);
+let compcada8hs="1";
+console.log(compcada8hs);
+let comppordia=(1*3);
+console.log(comppordia);
+
+let ingresomg=prompt("miligramos por comprimido");
+console.log(ingresomg);
+let miligramospordia= comppordia*ingresomg;
+console.log(miligramospordia)
+
